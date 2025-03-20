8b080be578f2a04bf038f149846bf76d087d305d
womimc <138317871+womimc@users.noreply.github.com>
2025-03-20 23:14:40 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,12 @@
 const mineflayer = require('mineflayer');
 const bot = mineflayer.createBot({
-    host: "006evx.aternos.me",
+    host: "example.com",
     port: 25565,
-    username: "zenXmnDpl",
+    username: "Womi-Bot",
     version: "1.12.2",
     loadInternalPlugins: false
 });
 bot.on('spawn', () => {
-    console.log("✅ Bot dołączył, wysyłam /trigger afk...");
-    bot.chat("/trigger afk");
+    console.log("✅ Joined!");
 });
-bot.on('error', (err) => console.log(`❌ Błąd: ${err}`));
-bot.on('end', () => console.log("⚠️ Bot został rozłączony, próbuję ponownie..."));
+bot.on('error', (err) => console.log(`❌ Error: ${err}`));
